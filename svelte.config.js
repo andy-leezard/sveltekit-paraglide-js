860a9035ebd6e40009b4a2bacaaa60a008c26ab7
Andy Lee <a.lee.intl@gmail.com>
2023-12-21 17:43:32 +0100

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/kit/vite"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+
+		alias: {
+			$paraglide: "./src/paraglide/",
+		},
+
+		// Need for crawling to work until
+		// https://github.com/sveltejs/kit/issues/11133
+		// is fixed
+		prerender: {
+			entries: ["/"],
+		},
+	},
+}
+
+export default config
